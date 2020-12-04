eed021f54448203c3f2b800da006fc5d9a705d5f
eryo12138 <15171271104@163.com>
2020-12-04 12:44:57 +0800

    算法课上跟写

--- AlgorithmCode/src/com/eryo/MinSumTree.java
+++ AlgorithmCode/src/com/eryo/MinSumTree.java
@@ -1,22 +1,45 @@
 package com.eryo;
 
-import java.util.logging.Level;
-
 /**
  *  author:18301092-陈佳林
  *  time:2020年12月2日
  */
 public class MinSumTree {
 
+    private static int minSum;
+    private static  TreeNode minRoot;
+
+    public static int findMinSumtree1(TreeNode root){
+        minSum = Integer.MAX_VALUE;
+        minRoot = null;
+        getSum(root);
+        return minSum;
+    }
+
+    private static int getSum(TreeNode root){
+        if (root == null){
+            return 0;
+        }
+
+        int sum = getSum(root.left) + getSum(root.right) + root.val;
+        if (sum < minSum)
+        {
+            minSum = sum;
+            minRoot = root;
+        }
+
+        return sum;
+    }
+
     private static int sum = Integer.MAX_VALUE;
 
-    public static int findMinSumTree(TreeNode root) {
+    public static int findMinSumTree2(TreeNode root) {
         if(root == null) {
             return 0;
         }
 
-        int left = findMinSumTree(root.left);
-        int right = findMinSumTree(root.right);
+        int left = findMinSumTree2(root.left);
+        int right = findMinSumTree2(root.right);
         sum = Math.min(sum, left + right + root.val);
         return sum;
     }
@@ -25,6 +48,11 @@ public class MinSumTree {
     public static void main(String[] args) {
         String str = "[1,2,3,null,5]";
         TreeNode root = TreeNode.stringToTreeNode(str);
-        System.out.println(findMinSumTree(root));
+
+        System.out.println("第一种方法");
+        System.out.println(findMinSumtree1(root));
+
+        System.out.println("第二种方法：");
+        System.out.println(findMinSumTree2(root));
     }
 }
